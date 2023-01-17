753981f5fec8e2a859dc5044351b1c142d045398
SaifBoubaker <saif.boubaker.contact@gmail.com>
2023-01-17 21:06:09 +0100

    hello

---
+++ index.js
@@ -0,0 +1,21 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+const PORT = 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
